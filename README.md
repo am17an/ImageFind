3d8a3b4acc65828927edc4fab9fddd2eef0ebb19
amangupta052 <amangupta052@gmail.com>
2013-11-07 18:03:26 +0530

    added command line things

--- README.md
+++ README.md
@@ -4,5 +4,4 @@ ImageFind
 Find Images through queries from command line and display them.
 It is pretty rudimentary, so please don't take it to heart. It has been tested on Mac OSX 10.8+, though
 with a few changes should work on linux too.
-I plan to add some command line options in the future.
 Have fun lolling!
--- imageFind.py
+++ imageFind.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python
-import os,json
+import os,json,sys
 from urllib import urlretrieve
+import string
+import random
+from optparse import OptionParser
+parser = OptionParser(usage="%prog 'search string' -d")
+parser.add_option("-d","--delete",action="store_true",default="False",dest="delete",help="delete after viewing image")
+if not len(sys.argv) > 1:
+  print 'Please specify search string as first argument'
+  sys.exit(0) 
 
-input_string = raw_input("Enter Search String \n")
-
+input_string = sys.argv[1]
+(options,args) = parser.parse_args()
 try:
   search_string = ('+').join(input_string.split())
   # Instead of my-ajax-site.com , please put your own identifier. Google TOS says so.
   json_response =  os.popen("curl -e http://www.my-ajax-site.com 'https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+search_string+"'").read()
   data = json.loads(json_response)
-  first_result = str(data["responseData"]["results"][0]["unescapedUrl"].decode("ascii"))
+  first_result = str(data["responseData"]["results"][random.randint(0,len(data["responseData"]["results"])-1)]["unescapedUrl"].decode("ascii"))
   image_format = first_result[first_result.find(".",len(first_result)-5,len(first_result)):]
-  image_name = '_'.join(input_string.split()) + image_format 
+  image_name = '_'.join(input_string.split())  
+  image_name += ''.join(random.choice(string.ascii_uppercase + string.digits) for i in xrange(6))
+  image_name += image_format
   urlretrieve(first_result,image_name)
   os.popen("qlmanage -p " + image_name) 
+  if options.delete == True:
+    os.popen("rm " + image_name)
 except Exception as e:
+  print e
   print "One or many things went wrong, let's have some muffins!"
 
 
